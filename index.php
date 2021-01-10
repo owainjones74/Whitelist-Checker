3c6cc7e303688cbad172006ddfda9a94df45f20e
Tomsci <62972629+itstomsci@users.noreply.github.com>
2021-01-10 08:32:35 +0000

    Added automatic loading to index

--- index.php
+++ index.php
@@ -41,6 +41,9 @@ if ($searchTerms == "") {
                 <tbody id="table">
                 </tbody>
             </table>
+            <div class="ui segment inverted" style="background-color: #2b2b2b; height: 600px;" id="loading">
+                <div class="ui active text loader">Loading Data</div>
+            </div>
             <?php } else { ?>
                 <div class="ui inverted segment" style="text-align: center;">
                     Please search for a job in the above text box! Alternatively, the job you entered may not be a valid whitelist!
@@ -48,41 +51,31 @@ if ($searchTerms == "") {
             <?php } ?>
 
             <script>
-                $("document").ready(function() {
-                    for(i = 0; i < 10; i++) {
+                $(document).ready(function() {
+                    $.get("http://localhost/Whitelist-Checker/api/job.php?job=<?php echo urlencode($searchTerms) ?>", function(data) {
+                        let result = Object.values(data);
                         let table = document.getElementById("table");
-                        let row = table.insertRow(-1);
-
-                        let cell1 = row.insertCell(0);
-
-                        cell1.innerHTML = "Test";
-                    };
-                    let table = document.getElementById("table");
-                    let row = table.insertRow(-1);
-                    row.insertCell(0);
-                    $('table').api({
-                        url: 'http://localhost/Whitelist-Checker/api/job.php?job=<?php echo urlencode($searchTerms) ?>',
-                        onResponse: function(response) {
-                            let result = [];
-                            let count = 10
-                            for(i = 0; i < Object.keys(response).length; i++) {
-                                result.push(response[i]);
-                            };
-                            $('table').visibility({
-                                once: false,
-                                observeChanges: true,
-                                onBottomVisible: function() {
-                                    for(i = 0; i < 10; i++) {
-                                        let table = document.getElementById("table");
-                                        let row = table.insertRow(-1);
-
-                                        let cell1 = row.insertCell(0);
-                                        cell1.innerHTML = result[i];
-                                        result.shift();
-                                    };
-                                }
-                            });
-                        }
+                        let loading = document.getElementById("loading");
+                        loading.remove();
+                        $('table').visibility({
+                            once: false,
+                            observeChanges: true,
+                            onBottomVisible: function() {
+                                for(i = 0; i < 20; i++) {
+                                    if(!result[0]) break;
+                                    let row = table.insertRow(-1);
+                                    let steamidCell = row.insertCell(0);
+                                    let linkCell = row.insertCell(1);
+                                    steamidCell.innerHTML = result[0];
+                                    linkCell.innerHTML = `<td style="text-align: center;">
+                                        <a href="https://thexyznetwork.xyz/profile/${result[0]}" class="ui inverted blue button">xSuite</a>
+                                        <a href="https://thexyznetwork.xyz/lookup/${result[0]}" class="ui inverted red  button">Lookup</a>
+                                        <a href="https://steamcommunity.com/profiles/${result[0]}" class="ui inverted grey button">Steam</a>
+                                    </td>`;
+                                    result.shift();
+                                };
+                            }
+                        });
                     });
                 });
             </script>
